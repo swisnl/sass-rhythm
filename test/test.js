699348e6774941e7f7048437c14b06d1c6ff526d
Anthony Schuijlenburg <aschuijlenburg@swis.nl>
2023-10-06 16:50:41 +0200

    feat: update tests to work with dart sass

--- test/test.js
+++ test/test.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const assert = require('assert');
 const sassTrue = require('sass-true');
-const describe = require("mocha");
 
 const sassFile = path.join(__dirname, 'test.scss');
-sassTrue.runSass({file: sassFile}, describe, it);
+sassTrue.runSass({ describe, it }, sassFile);
